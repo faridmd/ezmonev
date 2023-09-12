ba3545db712fa1add128da8251a167e7f326a42e
Farid <77087718+faridmd@users.noreply.github.com>
2023-09-12 10:24:49 +0700

    minor bug fixes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function ezmonev(){
     const n = 30;
     for(let i = 0; i <= n; i++ ) {
-        if(radio=document.evaluate(//input[@name="nilai[${i}]" and @value="5"],document,null,9,null).singleNodeValue)
-            radio.checked=true;
-    }
-
-const teks = 'Semoga kedepannya lebih baik'; 
-const textarea = document.querySelector('textarea');
-textarea.value = teks;
+        if(radio=document.evaluate(`//input[@name="nilai[${i}]" and @value="5"]`,document,null,9,null).singleNodeValue)
+            radio.checked=true;}
+    
+    const saran = 'Semoga ilmunya bermanfaat';	
+    const textarea = document.querySelector('textarea');
+    textarea.value = saran;
 }
+
 ezmonev();
