a2e2f3aa94112702935c30b031814bb0f8db6ee2
Farid Anwar <muyonowongcstr@gmail.com>
2024-03-14 14:05:57 +0700

    penambahan fitur nilai

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 function ezmonev(){
     const n = 30;
-    for(let i = 0; i <= n; i++ ) {
-        if(radio=document.evaluate(`//input[@name="nilai[${i}]" and @value="5"]`,document,null,9,null).singleNodeValue)
-            radio.checked=true;}
-    
+    let nilai = prompt('Mau nilainya berapa? contoh: 5')
+    for(let i = 0; i <= n; i++ ) 
+    {
+        if(radio=document.evaluate(`//input[@name="nilai[${i}]" and @value="${nilai}"]`,document,null,9,null).singleNodeValue)
+            radio.checked=true;
+    }
     const saran = 'Semoga ilmunya bermanfaat';	
     const textarea = document.querySelector('textarea');
     textarea.value = saran;
+    console.log('Sudah selesai!')
 }
 
 ezmonev();
